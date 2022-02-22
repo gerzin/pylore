0d6ce65752de7a1d228c4d9fa24aa8b57cd00c52
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-02-22 12:50:03 +0100

    Initial commit

---
+++ pylore/lore.py
@@ -0,0 +1,13 @@
+def indicator(condition: bool):
+    """
+    Indicator function which returns 1 if the condition is True else false.
+    :param condition:
+    :return: 1 if the condition is true else 0
+    """
+    return 1 if condition else 0
+
+def fitness_equal(x, z):
+    print("fitness")
+
+def fitness_nequal(x, z):
+    print("fitness")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='pylore',
+    version='0.0.1',
+    description='LOcal Rule-based Explanations',
+    packages=['pylore']
+)
