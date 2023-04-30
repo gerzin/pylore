abe59697d7f4c36649d9e00da23282d25cac6e79
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2023-04-30 23:16:19 +0200

    Add comment

--- pylore/blackbox.py
+++ pylore/blackbox.py
@@ -2,6 +2,13 @@ from abc import abstractmethod, ABC
 
 
 class AbstractBlackBoxWrapper(ABC):
+    """
+    Interface for using a black box with LORE.
+
+    No matter the framework (Pytorch, Keras, Tensorflow) just create a class which wraps the model
+    and exposes a `predict` method in order to obtain predictions on instances.
+    """
+
     @abstractmethod
-    def predict(self):
+    def predict(self, x):
         pass
