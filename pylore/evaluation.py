d3c41fff0664a9c114ebed23d98664aeb62418fb
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2023-05-04 23:26:18 +0200

    Add template

---
+++ pylore/evaluation.py
@@ -0,0 +1,24 @@
+"""
+This file contains the definition of utilities to evaluate
+the quality of a local explainer.
+"""
+
+
+def fidelity():
+    raise NotImplementedError
+
+
+def l_fidelity():
+    raise NotImplementedError
+
+
+def cl_fidelity():
+    raise NotImplementedError
+
+
+def hit():
+    raise NotImplementedError
+
+
+def c_hit():
+    raise NotImplementedError
